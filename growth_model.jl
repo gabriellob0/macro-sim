13d83bcf823feb0f610cf47b1090eb1d6ebb8d67
gabriellob0 <126202283+gabriellob0@users.noreply.github.com>
2024-10-17 15:41:14 +0100

    Add model parameters

--- growth_model.jl
+++ growth_model.jl
@@ -1,22 +1,54 @@
 using CairoMakie
 
 
+# Production function
+produce(cap, tech, pop, cap_elas = 0.3) = cap^cap_elas * (tech*pop)^(1-cap_elas)
+
+# Laws of motion
+motion_capital(cap, inc, dep = 0.5, savings = 0.2) = (1-dep)*cap + savings*inc
+motion_population(pop, growth_rate = 0) = (1+growth_rate)*pop
+motion_technology(tech, growth_rate = 0.2) = (1+growth_rate)*tech 
+
+
+# At the steady state, capital is (0.2 / (0.2 + 0.5))^(1 / (1 - 0.3)),
+# which equals (2/7)^(1/0.7) ~= 0.167
+
+
 # Model
-function sim_capital_motion(a = 0.5,d = 0.8, s = 0.5, K0 = 0.1, T = 20)
-    Y = zeros(T+1)
-    K = zeros(T+1)
-
-    K[1] = K0
-    Y[1] = K[1]^a
-
-    for t in 1:T
-        K[t+1] = (1 - d) * K[t] + s*Y[t]
-        Y[t+1] = K[t]^a
+function simulate_growth(periods = 10, initial_cap = 0.1, initial_pop = 1, initial_tech = 1)
+    Y = zeros(periods)
+    K = zeros(periods)
+    N = zeros(periods)
+    A = zeros(periods)
+    
+    K[1] = initial_cap
+    N[1] = initial_pop
+    A[1] = initial_tech
+    
+    for t in 1:periods
+        Y[t] = produce(K[t], A[t], N[t])
+        
+        if t < periods
+            K[t+1] = motion_capital(K[t], Y[t])
+            N[t+1] = motion_population(N[t])
+            A[t+1] = motion_technology(A[t])
+        end
     end
-
-    return Y
+    
+    return Y, K
 end
 
-Y = sim_capital_motion()
+scot_cap = 0.167
+eng_cap = 0.167 / 2
 
-lines(1:21, Y)
+Y_england, K_england = simulate_growth(10, eng_cap, 1, 1)
+Y_scotland, K_scotland = simulate_growth(10, scot_cap, 1, 1)
+
+# Plot evolution of income
+lines(1:10, Y_england, label = "England")
+lines!(1:10, Y_scotland, label = "Scotland")
+axislegend(position = :rb)
+current_figure()
+
+# Plot evolution of capital
+lines(1:10, K_scotland)
